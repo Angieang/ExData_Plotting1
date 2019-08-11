507c2a491ee1da34523323fa00adf214d2b2a0e6
Angelina <angieang9@gmail.com>
2019-08-11 16:02:37 +0300

    plot3.R

--- plot3.R
+++ plot3.R
@@ -5,8 +5,7 @@ plot(Powerfinal$Sub_metering_1~Powerfinal$Date_Time, type="l", ylab="Energy sub 
 points(Powerfinal$Sub_metering_2~Powerfinal$Date_Time, type="l", col="red")
 points(Powerfinal$Sub_metering_3~Powerfinal$Date_Time, type="l", col="blue")
 
-legend("topright", lwd = "1", col=c("black", "red", "blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-
+legend("topright", lty=1, lwd = 1, col=c("black", "red", "blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 
 ##Copy the plot as a png file named plot3.png, with a width of 480 pixels and a height of 480 pixels
@@ -14,6 +13,3 @@ dev.copy(png, file="plot3.png", width=480, height = 480)
 
 ##Close the png device
 dev.off()
-
-
-
