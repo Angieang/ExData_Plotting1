fed88d26f1275104942312a6963cbbed706b3ef2
Angelina <angieang9@gmail.com>
2019-08-11 18:31:04 +0300

    plot3.R

--- plot3.R
+++ plot3.R
@@ -10,5 +10,6 @@ legend("topright", lty=1, lwd = 1, col=c("black", "red", "blue"), legend=c("Sub_
 ##Copy the plot as a png file named plot3.png, with a width of 480 pixels and a height of 480 pixels
 dev.copy(png, file="plot3.png", width=480, height = 480)
 
+
 ##Close the png device
 dev.off()
